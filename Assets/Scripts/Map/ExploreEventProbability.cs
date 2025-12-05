be4d5bc42ac3b3fadfc22ad143dadd682e019e43
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2025-12-05 20:09:08 +0900

    ちょっと修正

--- Assets/Scripts/Map/ExploreEventProbability.cs
+++ Assets/Scripts/Map/ExploreEventProbability.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 public class ExploreEventProbability
 {
     public string eventName;
-    [Range(0f, 1f)]
-    public float probability;
+    [Range(0f, 1f)] public float probability;
+    public UnityEngine.Events.UnityEvent onEvent;
 }
