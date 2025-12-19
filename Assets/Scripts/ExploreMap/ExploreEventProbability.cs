38484c814d86fe11f7f300ce69cbe28933bb7c4a
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2025-12-19 19:53:10 +0900

    ちょっときれいにした

--- Assets/Scripts/ExploreMap/ExploreEventProbability.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class ExploreEventProbability
-{
-    public string eventName;
-    [Range(0f, 1f)] public float probability;
-    public UnityEngine.Events.UnityEvent onEvent;
-}
